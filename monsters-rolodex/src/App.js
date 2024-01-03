4448866d3f4fb56211af2b3429414acf42703f16
Ferenc Koler <ferenckoler@outlook.com>
2024-01-03 21:14:37 +0100

    Monsters Rolodex - Storing Original Data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
   };
 
@@ -29,25 +30,25 @@ class App extends Component {
 
   render() {
 
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
+
         <input className='search-box' type='search' placeholder='Search Monsters'
           onChange={(event) => {
-            console.log(event.target.value);
 
-            const searchString = event.target.value.toLowerCase();
-
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString);
-            });
+            const searchField = event.target.value.toLowerCase();
 
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
 
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>
