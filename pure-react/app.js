0e83b5e8d2900941ead572ffaa51971578b66ecf
Ferenc Koler <ferenckoler@outlook.com>
2024-01-06 15:19:44 +0100

    ReactDOM v18 Changes

--- pure-react/app.js
+++ pure-react/app.js
@@ -1,20 +1,19 @@
 const Person = (props) => {
     return React.createElement('div', {}, [
-        React.createElement('h1', {}, props.name),
-        React.createElement('p', {}, props.occupation),
+        React.createElement('h1', { key: 1 }, props.name),
+        React.createElement('p', { key: 2 }, props.occupation),
     ]);
 };
 
 const App = () => {
     return React.createElement('div', {}, [
-        React.createElement('h1', { className: 'title' }, 'React is rendered'),
-        React.createElement(Person, { name: 'Frank', occupation: 'WebDeveloper' }, null),
-        React.createElement(Person, { name: 'Jasna', occupation: 'UI/UX Designer' }, null),
-        React.createElement(Person, { name: 'Theodor', occupation: 'Student' }, null),
+        React.createElement('h1', { className: 'title', key: 3 }, 'React is rendered'),
+        React.createElement(Person, { name: 'Frank', occupation: 'WebDeveloper', key: 4 }, null),
+        React.createElement(Person, { name: 'Jasna', occupation: 'UI/UX Designer', key: 5 }, null),
+        React.createElement(Person, { name: 'Theodor', occupation: 'Student', key: 6 }, null),
     ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(React.createElement(App));
