210c4c7764ed5b5648155f66404b89c79e4fd6cc
Ferenc Koler <ferenckoler@outlook.com>
2024-01-08 22:44:17 +0100

    React Router Link

--- merch-shop/src/App.js
+++ merch-shop/src/App.js
@@ -1,18 +1,7 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return (
@@ -24,7 +13,7 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
