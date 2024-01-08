e876420452b76a335bc7066034f5598d9b8ff4a0
Ferenc Koler <ferenckoler@outlook.com>
2024-01-08 22:13:27 +0100

    Navigation Bar Component

--- merch-shop/src/App.js
+++ merch-shop/src/App.js
@@ -1,6 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
-import Home from './routes/home/home.component'
+import Home from './routes/home/home.component';
+
+const Navigation = () => {
+
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
   return (
@@ -12,7 +24,8 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
