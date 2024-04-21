c045b6198c6be14ef8b8921470d371dfc3dcb99f
Ferenc Koler <ferenckoler@outlook.com>
2024-04-21 23:20:19 +0200

    Cart Icon & Dropdown

--- merch-shop/src/components/product-card/product-card.component.jsx
+++ merch-shop/src/components/product-card/product-card.component.jsx
@@ -14,7 +14,7 @@ const ProductCard = ({ product }) => {
                 <span className='price'>{price}</span>
             </div>
 
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted'>Add to cart</Button>
         </div>
     );
 };
