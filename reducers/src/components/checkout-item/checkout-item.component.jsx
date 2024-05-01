80259ec9cbf94a6e83877a811277dccce56a1f8e
Ferenc Koler <ferenckoler@outlook.com>
2024-05-01 14:14:55 +0200

    User Reducer

---
+++ reducers/src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    BaseSpan,
+    Quantity,
+    Arrow,
+    Value,
+    RemoveButton,
+} from './checkout-item.styles';
+
+const CheckoutItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+        useContext(CartContext);
+
+    const clerItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemFromCart(cartItem);
+
+    return (
+        <CheckoutItemContainer>
+            <ImageContainer>
+                <img src={imageUrl} alt={`${name}`} />
+            </ImageContainer>
+
+            <BaseSpan> {name} </BaseSpan>
+
+            <Quantity>
+                <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+
+                <Value>{quantity}</Value>
+
+                <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+            </Quantity>
+
+            <BaseSpan> ${price} </BaseSpan>
+
+            <RemoveButton onClick={clerItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
+    );
+};
+
+export default CheckoutItem;
