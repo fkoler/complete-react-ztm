1304fab2775e32951e20c7874427d017f3faf148
Ferenc Koler <ferenckoler@outlook.com>
2024-05-03 12:23:35 +0200

    What Triggers useSelector

--- react-redux/src/store/categories/category.selector.js
+++ react-redux/src/store/categories/category.selector.js
@@ -1,7 +1,9 @@
-export const selectCategoriesMap = (state) =>
-    state.categories.categoriesArray.reduce((acc, category) => {
+export const selectCategoriesMap = (state) => {
+    console.log('selector fired');
+    return state.categories.categoriesArray.reduce((acc, category) => {
         const { title, items } = category;
 
         acc[title.toLowerCase()] = items;
         return acc;
     }, {});
+};
