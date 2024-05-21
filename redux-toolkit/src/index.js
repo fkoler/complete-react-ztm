93c24ed03c2829910f55f416113110da2efb34cd
Ferenc Koler <ferenckoler@outlook.com>
2024-05-21 13:12:14 +0200

    Using createSlice

--- redux-toolkit/src/index.js
+++ redux-toolkit/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store, persistor } from './store/store';
+import {
+    store,
+    // persistor
+} from './store/store';
 
 import './index.scss';
 
