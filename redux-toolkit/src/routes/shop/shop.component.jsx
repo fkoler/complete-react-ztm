36946e8fa7161983ed47ad22a0fd9458d0dc2c60
Ferenc Koler <ferenckoler@outlook.com>
2024-05-24 18:32:42 +0200

    Categories to createSlice

--- redux-toolkit/src/routes/shop/shop.component.jsx
+++ redux-toolkit/src/routes/shop/shop.component.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.reducer';
 
 const Shop = () => {
     const dispatch = useDispatch();
@@ -15,6 +15,7 @@ const Shop = () => {
             const categoriesArray = await getCategoriesAndDocuments(
                 'categories'
             );
+
             dispatch(setCategories(categoriesArray));
         };
 
