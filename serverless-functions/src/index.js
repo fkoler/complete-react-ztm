8cf6e40d5599a536698f3e0a1ef2c8c4f09735f0
Ferenc Koler <ferenckoler@outlook.com>
2024-06-16 18:57:18 +0200

    Setting up Stripe

---
+++ serverless-functions/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+import App from './App';
+import { store } from './store/store';
+
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
+
+reportWebVitals();
