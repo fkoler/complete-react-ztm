b96cfa60cee8eb034a00eb5026b0ccac194d19ff
Ferenc Koler <ferenckoler@outlook.com>
2024-07-13 21:51:27 +0200

    ChangeEvent Type From React

--- typescript-monsters-rolodex/src/components/search-box/search-box.component.tsx
+++ typescript-monsters-rolodex/src/components/search-box/search-box.component.tsx
@@ -1,11 +1,11 @@
-import { ChangeEventHandler } from 'react';
+import { ChangeEvent } from 'react';
 
 import './search-box.styles.css';
 
 type SeearchBoxProps = {
     className: string;
     placeholder?: string;
-    onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
 const SearchBox = ({
