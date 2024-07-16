f00e6d655f758b16da14b6c74b216f33b4ad97c8
Ferenc Koler <ferenckoler@outlook.com>
2024-07-16 15:10:18 +0200

    Typing Category Selectors

--- typescript-redux/src/store/categories/category.reducer.ts
+++ typescript-redux/src/store/categories/category.reducer.ts
@@ -1,6 +1,12 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { Category, CATEGORIES_ACTION_TYPES } from './category.types';
 
 import { CategoryAction } from './category.action';
+
+export type CategoriesState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
@@ -12,15 +18,17 @@ export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
   action = {} as CategoryAction
 ) => {
-  const { type, payload } = action;
 
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
+
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: payload, isLoading: false };
+      return { ...state, categories: action.payload, isLoading: false };
+
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: payload, isLoading: false };
+      return { ...state, error: action.payload, isLoading: false };
+
     default:
       return state;
   }
