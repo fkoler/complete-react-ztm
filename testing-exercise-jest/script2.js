52f3f938a9996cf4661924acbf5f314fa9f9c319
Ferenc Koler <ferenckoler@outlook.com>
2024-08-27 12:45:30 +0200

    Asynchronous Tests 2

--- testing-exercise-jest/script2.js
+++ testing-exercise-jest/script2.js
@@ -1,29 +1,35 @@
 // Now a little bit more realistic:
 const fetch = require('node-fetch');
 
+const url = 'http://swapi.py4e.com/api/people';
+
 const getPeoplePromise = (fetch) => {
-  return fetch('http://swapi.py4e.com/api/people')
-    .then(response => response.json())
-    .then(data => {
-      return {
-        count: data.count,
-        results: data.results
-      }
-    })
-}
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            return {
+                count: data.count,
+                results: data.results,
+            };
+        });
+};
+
 const getPeople = async (fetch) => {
-  const getRequest = await fetch('http://swapi.py4e.com/api/people');
-  const data =  await getRequest.json();
-  // console.log(data);
-  return {
-    count: data.count,
-    results: data.results
-  };
-}
-// getPeople();
-// getPeoplePromise();
+    const getRequest = await fetch(url);
+    const data = await getRequest.json();
+
+    console.log(data);
+
+    return {
+        count: data.count,
+        results: data.results,
+    };
+};
+
+getPeople(fetch);
+getPeoplePromise(fetch);
 
 module.exports = {
-  getPeople,
-  getPeoplePromise
-}
+    getPeoplePromise,
+    getPeople,
+};
